bdfc6689ac984a6290b6c88f4a2b9acef8d7d2d5
JkqzDev <espulefjuan13@gmail.com>
2022-11-06 05:05:45 -0300

    Update webhooks

--- src/uhc/discord/DiscordWebhook.php
+++ src/uhc/discord/DiscordWebhook.php
@@ -6,7 +6,7 @@ namespace uhc\discord;
 
 final class DiscordWebhook {
     
-    public const CHANNEL_ANNOUNCEMENTS = 'https://discord.com/api/webhooks/1037168149184909343/WCCZSQKZhDfzlhsIhNvyyyEFfo2YY6kwt_oIaLTqxSbKI4vY_qG0HQBU-JDCtmZh0czC';
-    public const CHANNEL_KILLS = 'https://discord.com/api/webhooks/1037168214427308092/q9ib6orVllC0Y2NsRtE6-9I7yPGlrz0m6quMwHtNEgWaVRkj5BWAFYo8z9pzMqoeRvUv';
-    public const CHANNEL_WINNERS = 'https://discord.com/api/webhooks/1037168311986815049/4XPpC9G5ArgxQi6Cl5e8gj7DEWexgfHP1jUAweDVxHYAsIlk-WV0ci8utW6iHZtCD6yH';
+    public const CHANNEL_ANNOUNCEMENTS = 'https://discord.com/api/webhooks/1038907512692420628/IyfXMH6bFJTo_8RHQwiEvkjefWbI8_B8LWtLTOy3PFObEjRN42HDbyu7nNmpaPVe-bQS';
+    public const CHANNEL_KILLS = 'https://discord.com/api/webhooks/1038907653889462312/jIiYJxljzKL24G-xKuDPLv5MugfhsJl7dt9A2QyK4XOVsIBjHRDKAHtfqHW0-rfgV740';
+    public const CHANNEL_WINNERS = 'https://discord.com/api/webhooks/1038907713398263920/pGt1ixCHTfNDXWDywV4yQjM1mCZXxB5pymYJKk8SO9x48m5MAnU2ag4VcyCRPXi46LIN';
 }
